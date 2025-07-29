93aaa7fcb4a3fa6a57a614737d33956e82e141c2
Hirun4 <hirun08072002@gmail.com>
2025-07-29 14:46:34 +0530

    refactor: Clean up comments and whitespace in app configuration file

--- chatapp/lib/config/app_config.dart
+++ chatapp/lib/config/app_config.dart
@@ -6,32 +6,32 @@ class AppConfig {
 
   static const String _serverPort = '8000';
 
-  // Base URLs
+  
   static const String baseUrl = 'http://$_serverHost:$_serverPort';
   static const String apiUrl = '$baseUrl/api';
   static const String socketUrl = baseUrl;
 
-  // Socket.IO Configuration
+
   static const Map<String, dynamic> socketOptions = {
     'transports': ['websocket'],
     'autoConnect': false,
   };
 
-  // Network Configuration
-  static const int connectionTimeout = 10000; // 10 seconds
-  static const int receiveTimeout = 10000; // 10 seconds
+  
+  static const int connectionTimeout = 10000; 
+  static const int receiveTimeout = 10000; 
 
-  // Debug Settings
+ 
   static const bool enableDebugLogs = true;
 
-  // App Information
+  
   static const String appName = 'University Chat App';
   static const String appVersion = '1.0.0';
 }
 
-// Environment-specific configurations
+
 class DevConfig extends AppConfig {
-  static const String baseUrl = 'http://10.0.2.2:8000'; // Emulator
+  static const String baseUrl = 'http://10.0.2.2:8000'; 
   static const String apiUrl = '$baseUrl/api';
 }
 
@@ -40,9 +40,9 @@ class ProdConfig extends AppConfig {
   static const String apiUrl = '$baseUrl/api';
 }
 
-// Helper class to get current environment config
+
 class Config {
-  static const bool isDevelopment = true; // Change this for production
+  static const bool isDevelopment = true; 
 
   static String get baseUrl =>
       isDevelopment ? DevConfig.baseUrl : ProdConfig.baseUrl;
